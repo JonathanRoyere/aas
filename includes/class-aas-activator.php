aa731c6b024bf09a3a99b9e9665211f62f7fd56f
Jonathan Royere <jonathan@Jonathans-MacBook-Pro.local>
2017-08-28 20:12:42 -0700

    initial commit

---
+++ includes/class-aas-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       panda-ant.com
+ * @since      1.0.0
+ *
+ * @package    Aas
+ * @subpackage Aas/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Aas
+ * @subpackage Aas/includes
+ * @author     Jonathan Royere <jonathan.royere@gmail.com>
+ */
+class Aas_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-aas-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       panda-ant.com
+ * @since      1.0.0
+ *
+ * @package    Aas
+ * @subpackage Aas/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Aas
+ * @subpackage Aas/includes
+ * @author     Jonathan Royere <jonathan.royere@gmail.com>
+ */
+class Aas_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
